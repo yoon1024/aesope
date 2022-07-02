9be81e78fc87b34018e0dd65b4a22da650829e28
yoon1024 <1anvin81@gmail.com>
2022-07-03 02:29:06 +0900

    갤러리

--- js/gallery.js
+++ js/gallery.js
@@ -8,23 +8,102 @@
 
 //passowrd : 6226e7c4405d4252
 
+//flickr.photos.search
+
 
 const base = "https://www.flickr.com/services/rest/?";
 const method = "flickr.interestingness.getList";
+const method2 = "flickr.photos.search";
 const key = "ca74b70b0051720330ff8c2bef418cbf";
-const per_page = 100;
+const per_page = 30;
 const format = "json";
 
-const url = `${base}method=${method}&api_key=${key}&per_page=${per_page}&format=${format}&nojsoncllback=1`;
+const url = `${base}method=${method2}&api_key=${key}&per_page=${per_page}&format=${format}&nojsoncallback=1&tags=beige&privacy_filter=1`;
+
+const frame = document.querySelector("#list");
+const loading = document.querySelector(".loading");
+
+const input = document.querySelector("#search");
+const btn = document.querySelector(".btnSearch")
+
+createList(url);
+
+btn.addEventListener("click",()=>{
+
+    let tag = input.value;
+    const urlsearch = `${base}method=${method2}&api_key=${key}&per_page=${per_page}&format=${format}&nojsoncallback=1&tags=${tag}&privacy_filter=1`
+    createList(urlsearch)
+
+})
 
 
-fetch(url)
+
+
+function createList(url){
+    fetch(url)
 
 .then(data=>{
-    let result = data.json();
+    let result = data.json(url);
     return result;
 })
 
 .then(json=>{
     let items = json.photos.photo;
+
+
+    htmls = '';
+
+    items.map((data)=>{
+        console.log(data)
+
+        let imgSrc = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`;
+
+        let imgSrcBig = `https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`;
+
+        htmls += `
+            <li class="item">
+                <div><a href="${imgSrcBig}">
+                    <img src="${imgSrc}" alt="">
+                    </a>
+                    <p>${data.title}</p>
+                </div>
+            </li>
+        `
+    })
+    frame.innerHTML = htmls;
+
+    const imgs = frame.querySelectorAll("img")
+
+    const len = imgs.length;
+
+    let count = 0;
+
+    for(let el of imgs){
+        el.onload = ()=>{
+
+            count++;
+            if(count === len) isoLayout();
+        }
+
+        el.onerror = (e)=>{
+            e.currentTarget.closest(".item").style.diplay = "none";
+        }
+    }
 })
+
+}
+
+
+
+function isoLayout(){
+
+    frame.classList.add("on");
+    loading.classList.add("off");
+
+    new Isotope("#list",{
+        itemSelector : ".item",
+        columWidth : ".item",
+        transitionDuration : "0.5s"
+    });
+
+}
