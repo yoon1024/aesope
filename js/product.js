a4191b9754f6934a74a15b1cd305131c5f3945c7
yoon1024 <1anvin81@gmail.com>
2022-07-07 01:15:59 +0900

    product버튼

--- js/product.js
+++ js/product.js
@@ -2,28 +2,20 @@ const dropdown = document.querySelector('.dropdown');
 const toggleButton = document.querySelector('.dropdown-toggle');
 const menu = document.querySelector('.dropdown-menu');
 const options = document.querySelectorAll('.dropdown-option');
-// const nextButton = document.querySelector('.next-button');
-
-// 토글버튼을 클릭 ->메뉴를 보여준다
-//메뉴 max-height:0
-//메뉴.show =>해지
+const skin = document.querySelector('.skin');
 
 toggleButton.addEventListener('click', function () {
-	menu.classList.toggle('show');
+	menu.classList.toggle('on');
 });
 
 toggleButton.addEventListener('blur', function () {
-	menu.classList.remove('show');
+	menu.classList.remove('on');
 });
 
 options.forEach(function (item) {
 	item.addEventListener('click', function (e) {
-		console.log(e.currentTarget);
-		const value = e.currentTarget.textContent.trim();
-		toggleButton.textContent = value;
+		const value = e.currentTarget.innerText;
+		toggleButton.innerText = value;
 		toggleButton.classList.add('selected');
-		// menu.classList.remove('show');
-
-		// nextButton.removeAttribute('disabled');
 	});
 });
